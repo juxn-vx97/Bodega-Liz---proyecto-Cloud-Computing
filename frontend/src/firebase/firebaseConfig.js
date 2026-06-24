4087f6fee66ce503296b9eba2c24a0541e69046e
juxn-vx97 <juansandoval1311@gmail.com>
2026-06-24 05:18:41 -0500

    Integracion Firebase Firestore y configuracion inicial

---
+++ frontend/src/firebase/firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+  
+  apiKey: "AIzaSyDeGiFsXPsGQ3WizOYRm-i2LDTiaUSwYcU",
+
+  authDomain: "bodega-liz.firebaseapp.com",
+
+  projectId: "bodega-liz",
+
+  storageBucket: "bodega-liz.firebasestorage.app",
+
+  messagingSenderId: "34759995952",
+
+  appId: "1:34759995952:web:6bceec6172dcb34605b3f5",
+
+  measurementId: "G-2FMQ1C6CE4"
+  
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
