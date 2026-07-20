5d4d11674ab930603f575d870be2511e1c6c7b6b
juxn-vx97 <juansandoval1311@gmail.com>
2026-07-19 20:14:19 -0500

    Eliminar persistencia del carrito en localStorage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
-
-import { useState, useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { useState } from "react";import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Productos from './pages/Productos'
@@ -14,25 +12,7 @@ import Recargas from "./pages/Recargas"
 
 function App() {
 
-const [carrito, setCarrito] = useState(() => {
-
-  const carritoGuardado =
-    localStorage.getItem('carrito')
-
-  return carritoGuardado
-    ? JSON.parse(carritoGuardado)
-    : []
-
-})
-
-useEffect(() => {
-
-  localStorage.setItem(
-    'carrito',
-    JSON.stringify(carrito)
-  )
-
-}, [carrito])
+const [carrito, setCarrito] = useState([]);
 
   return (
 
